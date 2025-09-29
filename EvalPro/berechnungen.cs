4e77894be1bf2f1b0255a5ffd2c68f303ff096a1
Bernhard, Goetz <bernhard_goetz@gmx.de>
2025-09-29 14:51:50 +0200

    berechnungen.cs - create class ap1

--- EvalPro/berechnungen.cs
+++ EvalPro/berechnungen.cs
@@ -1,9 +1,20 @@
+using system;
+
 class berechnungen
 {
-    int ap1;
-    int ap2schriftlich;
-    int ap2muendlich;
-    int ap2schriftlichbwl;
-    int ap2schriftlichpug;
-    int ap2schriftlichprog;
+    private int ap1;
+    private int ap2schriftlich;
+    private int ap2muendlich;
+    private int ap2schriftlichbwl;
+    private int ap2schriftlichpug;
+    private int ap2schriftlichprog;
 }
+
+class ap1
+{
+    private int ap1gewichtung;
+}
+
+set ap1gewichtung = 50;
+
+Console.WriteLine("ap1gewichtung");
