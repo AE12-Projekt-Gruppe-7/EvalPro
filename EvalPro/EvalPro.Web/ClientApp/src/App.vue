af5023b4f9bd7cbef30c74ee840e24bd9c811f82
aucheinmensch1 <zindler.paul06@gmail.com>
2025-09-30 15:44:02 +0200

    Added Stadt Regensburg Logo, Table

--- EvalPro/EvalPro.Web/ClientApp/src/App.vue
+++ EvalPro/EvalPro.Web/ClientApp/src/App.vue
@@ -1,11 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';
+import Row from 'primevue/row';
+
+const products = ref();
+const selectedProduct = ref();
+
+const visible = ref(false)
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+  <div class="header">
+    <img alt="Vue logo" class="logo" src="/../ClientApp/src/regensburg_logo.webp" width="full" height="100"/>
+  </div>
+    <div class="main-card">
+      <div class="card">
+        <DataTable class="table" v-model:selection="selectedProduct" :value="products" selectionMode="single" dataKey="id" :metaKeySelection="false"
+                @rowSelect="onRowSelect" @rowUnselect="onRowUnselect" >
+            <Column field="student_id" header="ID"></Column>
+            <Column field="student_name" header="Name"></Column>
+            <Column field="student_work" header="Ausbildungsbetrieb"></Column>
+        </DataTable>
+        <Toast/>
+      </div>
+
+      <h1>test</h1>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+  .header {
+    display: flex;
+    align-items: center;
+    background-color: rgb(229, 229, 229);
+  }
+
+  .main-card {
+    
+  }
+
+  .logo {
+    margin-left: 5%;
+    margin-bottom: 2rem;
+   margin-top: 1rem;
+  }
+
+  .table {
+    margin-top: 2rem;
+    width: 40%;
+  }
+</style>
--- EvalPro/EvalPro.Web/ClientApp/src/main.ts
+++ EvalPro/EvalPro.Web/ClientApp/src/main.ts
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import Lara from '@primeuix/themes/lara';
+import 'primeicons/primeicons.css'
+import PrimeVue from 'primevue/config';
+ 
 
 const app = createApp(App)
-
-app.use(router)
-
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara,
+    }
+});
 app.mount('#app')
