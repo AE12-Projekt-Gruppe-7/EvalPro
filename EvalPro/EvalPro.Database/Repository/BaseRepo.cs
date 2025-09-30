9041e6cfb66e9b175bd2cc422da61996432ac49b
openblocki <58093113+openblocki@users.noreply.github.com>
2025-09-30 12:26:46 +0200

    Test Json Systems Add Base Repo Class

---
+++ EvalPro/EvalPro.Database/Repository/BaseRepo.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace EvalPro.Database.Repository;
+
+public class BaseRepo
+{
+    public JsonWriter Writer;
+
+    public BaseRepo(string path)
+    {
+        var sw = new StreamWriter(path);
+        Writer = new JsonTextWriter(sw);
+    }
+}
---
+++ EvalPro/EvalPro.Database/Repository/ItemRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EvalPro.Database.Repository;
+
+public class ItemRepository
+{
+    private BaseRepo repo;
+
+    public ItemRepository()
+    {
+        repo = new BaseRepo("../../../jsons/items.json");
+    }
+}
