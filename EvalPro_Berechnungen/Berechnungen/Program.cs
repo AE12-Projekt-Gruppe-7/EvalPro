830431e02a915da7e8364027f372a29f80bdab58
Bernhard, Goetz <bernhard_goetz@gmx.de>
2025-09-30 12:01:01 +0200

    Program.cs - Create Logic for noteneingabe

--- EvalPro_Berechnungen/Berechnungen/Program.cs
+++ EvalPro_Berechnungen/Berechnungen/Program.cs
@@ -1,3 +1,39 @@
 ﻿using System;
 
-Console.WriteLine("test");
+namespace Berechnung
+{
+    class Berechnung
+    {
+        public int ap1 = 0;
+        public int ap2schriftlich = 0;
+        public int ap2muendlich = 0;
+        public int ap2schriftlichbwl = 0;
+        public int ap2schriftlichpug = 0;
+        public int ap2schriftlichprog = 0;
+
+        static void Main()
+        {
+            Console.WriteLine("Gebe die Note der AP1 ein: ");
+            string ap1s = Console.ReadLine();
+            int AP1 = Convert.ToInt32(ap1s);
+            int notehigh = 6;
+            if (AP1 > notehigh)
+            {
+                Console.WriteLine("Note ist nicht im erlaubten bereich!");
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                Console.WriteLine("Gebe die Note der AP2 schriftlich ein: ");
+                string ap2s = Console.ReadLine();
+                int ap2schriftlich = Convert.ToInt32(ap2s);
+                if (ap2schriftlich > notehigh)
+                {
+                    Console.WriteLine("Note ist nicht im erlaubten bereich!");
+                    Thread.Sleep(1000);    
+                }
+            }
+        }
+    }
+
+}
