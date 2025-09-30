4e72ca3757746994666fa6ae605480de3f676fdf
Bernhard, Goetz <bernhard_goetz@gmx.de>
2025-09-30 11:06:42 +0200

    berechnungen.cs - created in new folder

---
+++ EvalPro_Berechnungen/berechnungen.cs
@@ -0,0 +1,26 @@
+using system;
+
+
+namespace berechnung {
+    class berechnung
+    {
+        private int ap1;
+        private int ap2schriftlich;
+        private int ap2muendlich;
+        private int ap2schriftlichbwl;
+        private int ap2schriftlichpug;
+        private int ap2schriftlichprog;
+    }
+
+    class ap1
+    {
+        private int ap1gewichtung;
+        static void main(string[]args)
+        {
+            ap1gewichtung = 50;
+            Console.WriteLine("ap1gewichtung");
+        }
+    }
+    
+}
+
