5dc2a95c45350413e7e2c17daa979bd6a11bf4f2
paul-zindler-eventanizer <paul.zindler@x-plizit.de>
2025-10-06 15:14:56 +0200

    [Added] Header, Edit buttons, Delete buttons, new column button [Updated] better styling, index title

--- EvalPro/EvalPro.Web/ClientApp/src/App.vue
+++ EvalPro/EvalPro.Web/ClientApp/src/App.vue
@@ -1,57 +1,112 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue'
 
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import ColumnGroup from 'primevue/columngroup';
-import Row from 'primevue/row';
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup'
+import Row from 'primevue/row'
 
-const products = ref();
-const selectedProduct = ref();
+import Button from 'primevue/button'
+
+onMounted(() => {
+  products.value = [
+    //Schüler für die Liste
+    { id: '1', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '3', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '4', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '5', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '6', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '7', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '8', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '9', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '10', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '11', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '12', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '13', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '14', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '15', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+    { id: '16', name: 'Paul', last_name: 'Zindler', work: 'x-plizit' },
+  ]
+})
+
+const products = ref()
+const selectedProduct = ref()
+
+function editStudent(id: any) {
+  console.log('test')
+}
 
 const visible = ref(false)
 </script>
 
 <template>
-
   <div class="header">
-    <img alt="Vue logo" class="logo" src="/../ClientApp/src/regensburg_logo.webp" width="full" height="100"/>
-  </div>
-    <div class="main-card">
-      <div class="card">
-        <DataTable class="table" v-model:selection="selectedProduct" :value="products" selectionMode="single" dataKey="id" :metaKeySelection="false"
-                @rowSelect="onRowSelect" @rowUnselect="onRowUnselect" >
-            <Column field="student_id" header="ID"></Column>
-            <Column field="student_name" header="Name"></Column>
-            <Column field="student_work" header="Ausbildungsbetrieb"></Column>
-        </DataTable>
-        <Toast/>
-      </div>
+    <!-- Header mit Logo -->
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="/../ClientApp/src/regensburg_logo.webp"
+      width="full"
+      height="100"
+    />
 
-      <h1>test</h1>
+    <h1 class="header-text">EvalPro Tool</h1>
+  </div>
+
+  <div class="button-row">
+    <!-- Button Reihe (Ausgewählte löschen, Auswahl zurücksetzen, Neuer Eintrag) -->
+    <Button
+      icon="pi pi-trash"
+      label="Ausgewählte löschen"
+      style="background-color: red; border-color: red"
+      disabled
+    />
+
+    <Button
+      icon="pi pi-ban"
+      label="Auswahl zurücksetzen"
+      style="background-color: darkblue; border-color: darkblue"
+      disabled
+    />
+
+    <Button icon="pi pi-plus" label="Neuer Eintrag" />
+  </div>
+
+  <div class="main-card">
+    <div class="card">
+      <!-- Schülerliste -->
+      <DataTable
+        class="table"
+        v-model:selection="selectedProduct"
+        :value="products"
+        selectionMode="multiple"
+        dataKey="id"
+        :metaKeySelection="false"
+        scrollable
+        scrollHeight="800px"
+      >
+        <Column field="id" header="ID"></Column>
+        <Column field="name" header="Vorname"></Column>
+        <Column field="last_name" header="Nachname"></Column>
+        <Column field="work" header="Ausbildungsbetrieb"></Column>
+        <Column style="width: 5%">
+          <template #body="slotProps">
+            <Button icon="pi pi-pencil" @click="editStudent(slotProps.data.id)" />
+          </template>
+        </Column>
+        <Column style="width: 10%">
+          <template #body="slotProps">
+            <Button
+              icon="pi pi-trash"
+              @click="deleteStudent(slotProps.data.id)"
+              style="background-color: red; border-color: red"
+            />
+          </template>
+        </Column>
+      </DataTable>
+      <Toast />
     </div>
+  </div>
 </template>
 
-<style scoped>
-
-  .header {
-    display: flex;
-    align-items: center;
-    background-color: rgb(229, 229, 229);
-  }
-
-  .main-card {
-    
-  }
-
-  .logo {
-    margin-left: 5%;
-    margin-bottom: 2rem;
-   margin-top: 1rem;
-  }
-
-  .table {
-    margin-top: 2rem;
-    width: 40%;
-  }
-</style>
+<style scoped></style>
--- EvalPro/EvalPro.Web/ClientApp/src/main.ts
+++ EvalPro/EvalPro.Web/ClientApp/src/main.ts
@@ -1,14 +1,19 @@
+import './main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import Lara from '@primeuix/themes/lara';
 import 'primeicons/primeicons.css'
-import PrimeVue from 'primevue/config';
- 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import type { Plugin } from 'vue'
+import router from './router'
 
 const app = createApp(App)
-app.use(PrimeVue, {
-    theme: {
-        preset: Lara,
-    }
-});
+
+app.use(router)
+app.use(PrimeVue as unknown as Plugin, {
+  theme: {
+    preset: Aura,
+  },
+})
 app.mount('#app')
